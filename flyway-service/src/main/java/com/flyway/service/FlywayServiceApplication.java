83844fe0ddb6854dc55af46c453405cd90a023a0
dharmendramore <info.dharmendra2221@gmail.com>
2026-01-16 00:36:55 +0530

    Added first commit in flyway service

---
+++ flyway-service/src/main/java/com/flyway/service/FlywayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flyway.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayServiceApplication.class, args);
+	}
+
+}
